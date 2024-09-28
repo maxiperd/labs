ba2d12a41bc5bc0fcbae1203168381ba470b367a
maxiperd <86020042+maxiperd@users.noreply.github.com>
2024-09-28 10:15:30 -0300

    Create searchUsers.php

---
+++ web/searchUsers.php
@@ -0,0 +1,14 @@
+<?php>
+  $servername = "mysql_db";
+  $username = "testuser";
+  $password = "testpassword";
+  $dbname = "testdb";
+  $conn = new mysqli($servername, $username, $password, $dbname);
+
+  $id = mysqli_real_escape_string($conn, $_GET('id'));
+  $data = mysqli_query($conn, "select username from users where id = $id");
+  $response = mysqli_fetch_array($data);
+  if(!isset($response['username'])){
+    http_response_code(404)
+  }
+<?>
